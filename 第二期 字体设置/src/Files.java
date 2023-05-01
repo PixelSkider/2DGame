174d114e870d9eb743d106f518663a2494f742f2
MiFea <MingLoL@foxmail.com>
2023-05-01 18:19:04 +0800

    Add files via upload

---
+++ 第二期 字体设置/src/Files.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Layiur!!!
+ * @create: 2023/5/1
+ * @Description:
+ * @FileName: Files
+ * @History:
+ */
+public class Files {
+    public String getPath(){
+        return System.getProperty("user.dir");
+    }
+}
